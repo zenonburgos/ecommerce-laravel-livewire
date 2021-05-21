8255ce4d51b65fea8571bd41013d486de333e0c6
zenonburgos <znburgos@gmail.com>
2021-05-21 16:08:11 -0600

    Trabajando header

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,3 +1,8 @@
-<div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+<div class="flex-1 relative">
+    <x-jet-input type="text" class="w-full" placeholder="¿Estás buscando algún producto?" />
+
+    <button class="absolute top-0 right-0 w-12 h-full bg-orange-500 flex items-center justify-center rounded-r-md">
+        <x-search size="35" color="white" />
+    </button>
+    
 </div>
